9ea528e50382bf82f324d3b8b92e827a2291b2e7
Sheshanath <35036388+sheshanathkumar@users.noreply.github.com>
2022-09-12 00:26:09 +0530

    enable dark mode

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,78 +1,26 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import { useState } from "react";
+import Home from "./Home";
 
 function App() {
-  const [text, setText] = useState("");
-  const [wordCount, setWordCount] = useState(0);
-  const [letterCount, setLetterCount] = useState(0);
+  const [color, setColor] = useState("");
 
-  const handleOnChange = (e) => {
-    setText(e.target.value);
-    setWordCount(countWord);
-    setLetterCount(countLetter);
-  };
+  const colorMode = (data) => {
+    console.log(data);
+    if (data === "dark") {
+      setColor("light");
+    } else {
+      setColor("dark");
+    }
 
-  const handleOnClickUpperCase = () => {
-    let textVal = text.toUpperCase();
-    setText(textVal);
-  };
-
-  const handleOnClickLowerCase = () => {
-    let textVal = text.toLowerCase();
-    setText(textVal);
-  };
-
-  const countWord = () => {
-    const arr = text.split(" ");
-    return arr.filter((w) => w !== "").length;
-  };
-
-  const countLetter = () => {
-    return text.length;
+    return data;
   };
 
   return (
     <>
-      <Navbar name="Text-Utils" />
-      <div className="container">
-        <div class="mb-3">
-          <label for="input-text" class="form-label my-3">
-            <h3>Enter your Text here</h3>
-          </label>
-          <textarea
-            class="form-control"
-            id="inputText"
-            rows="10"
-            value={text}
-            onChange={handleOnChange}
-          ></textarea>
-          <button
-            type="button"
-            class="btn btn-primary my-3"
-            onClick={handleOnClickUpperCase}
-          >
-            Upper Case
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-primary my-3 mx-4"
-            onClick={handleOnClickLowerCase}
-          >
-            Lower Case
-          </button>
-
-          <div className="container my-3">
-            <h3>Text Summary</h3>
-            <p>word count = {wordCount}</p>
-            <p>letter count = {letterCount} </p>
-
-            <h3>Whole Text</h3>
-            <p>{text}</p>
-          </div>
-        </div>
-      </div>
+      <Navbar name="Text-Utils" color={colorMode} />
+      <Home color={color} />
     </>
   );
 }
---
+++ textutils/src/Home.js
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+
+import validator from "validator";
+
+export default function Home(props) {
+  const [text, setText] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+  const [letterCount, setLetterCount] = useState(0);
+  const [validEmail, setValidEmail] = useState(false);
+
+  const currentColor = props.color;
+
+  console.log(props);
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+    setWordCount(countWord);
+    setLetterCount(countLetter);
+  };
+
+  const darkMode = {
+    height: "100vh",
+    backgroundColor: "#000000d6",
+    color: "White",
+  };
+
+  const lightMode = {
+    height: "100vh",
+    backgroundColor: "white",
+    color: "Black",
+  };
+
+  const getCurrentColorScheme = () => {
+    if (currentColor === "dark") {
+      return darkMode;
+    } else {
+      return lightMode;
+    }
+  };
+
+  const handleOnClickUpperCase = () => {
+    let textVal = text.toUpperCase();
+    setText(textVal);
+  };
+
+  const handleOnClickLowerCase = () => {
+    let textVal = text.toLowerCase();
+    setText(textVal);
+  };
+
+  const countWord = () => {
+    const arr = text.split(" ");
+    return arr.filter((w) => w !== "").length;
+  };
+
+  const countLetter = () => {
+    return text.length;
+  };
+
+  const handleOnClickClearText = () => {
+    setText("");
+    setWordCount(0);
+    setLetterCount(0);
+  };
+
+  const verifyEmail = () => {
+    console.log(text + " " + validator.isEmail(text));
+    if (validator.isEmail(text) === true) {
+      setValidEmail(true);
+      setText(text + " = " + validator.isEmail(text));
+    } else {
+      setValidEmail(false);
+      setText(text + " = " + validator.isEmail(text));
+    }
+  };
+
+  return (
+    <div style={currentColor === "dark" ? darkMode : lightMode}>
+      <div className="container">
+        <div className="mb-3">
+          <label htmlFor="input-text" className="form-label my-3">
+            <h3>Enter your Text here!</h3>
+          </label>
+          <textarea
+            className="form-control"
+            id="inputText"
+            rows="10"
+            value={text}
+            onChange={handleOnChange}
+            style={
+              currentColor === "dark"
+                ? { background: "#000000d6", color: "white" }
+                : { background: "white", color: "black" }
+            }
+          ></textarea>
+          <div className="gap-2 col-6 my-3">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleOnClickUpperCase}
+            >
+              Upper Case
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-primary mx-3"
+              onClick={handleOnClickLowerCase}
+            >
+              Lower Case
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={verifyEmail}
+            >
+              Check Email is Valid
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-secondary mx-3"
+              onClick={handleOnClickClearText}
+            >
+              Clear Text
+            </button>
+          </div>
+
+          <div className="container my-3">
+            <h3>Text Summary</h3>
+            <p>word count = {wordCount}</p>
+            <p>letter count = {letterCount} </p>
+
+            <h3>Whole Text</h3>
+            <p>{text}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
